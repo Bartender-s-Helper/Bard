01335eb38e6bcc3ac10c0824f3264d12b13d42bf
Lanse <simon_stalnabb@hotmail.com>
2023-06-27 20:26:08 +0200

    Push

---
+++ Bard/Data/Alcohol.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bard.Data
+{
+    public class Alcohol
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal ABV { get; set; }
+        public string? Description { get; set; }
+        public string? Country { get; set; }
+        public string? Categories { get; set; }
+        public string? SugarContents { get; set; }
+        public string? TastingNotes { get; set; }
+        public string? Producer { get; set; }
+    }
+}
